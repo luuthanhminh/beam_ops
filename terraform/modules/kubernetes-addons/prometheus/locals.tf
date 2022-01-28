2e810d4f58649f25f1fc39d645eeee26ca5f339e
Minh <minhltuit@gmail.com>
2022-01-28 21:40:43 +0700

    update prometheus version

--- terraform/modules/kubernetes-addons/prometheus/locals.tf
+++ terraform/modules/kubernetes-addons/prometheus/locals.tf
@@ -3,7 +3,7 @@ locals {
     name                       = "prometheus"
     chart                      = "prometheus"
     repository                 = "https://prometheus-community.github.io/helm-charts"
-    version                    = "14.4.0"
+    version                    = "15.1.1"
     namespace                  = "prometheus"
     timeout                    = "300"
     create_namespace           = false
